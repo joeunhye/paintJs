d1ca9911202fde8cf440a6be1b210dff640ee952
joeunhye <masimel33@gmail.com>
2022-01-30 16:12:31 +0900

    save 기능 추가

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const save = document.getElementById('jsSave');
 const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+ctx.fillStyle = 'white';
+ctx.fillRect(0,0,canvas.width,canvas.height)
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -61,12 +64,25 @@ function handleCanvasClick() {
         ctx.fillRect(0,0,canvas.width,canvas.height)
     }
 }
+function handleCM(e) {
+    //console.log(e);
+    e.preventDefault();
+}
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'paintJs';
+    //console.log(link)
+    link.click()
+}
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
     canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM)
 }
 //console.log(Array.from(colors)) // object로부터 array를 만든다.
 Array.from(colors).forEach((color) => {
@@ -78,4 +94,7 @@ if(range) {
 }
 if(mode) {
     mode.addEventListener('click', handleModeClick)
+}
+if(save) {
+    save.addEventListener('click', handleSaveClick)
 }
